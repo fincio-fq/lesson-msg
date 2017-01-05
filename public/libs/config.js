0f2f3ea8fc8601650cd5522dbce9c99c0417dd69
fincio <fq0186@hotmail.com>
2017-01-05 17:25:56 +0800

    登录完成

---
+++ public/libs/config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by fincio on 2017/1/5.
+ */
+
+require.config({
+    baseUrl: '/studyit1/public',
+    paths: {
+        jquery: 'assets/jquery/jquery',
+        cookie: 'assets/jquery-cookie/jquery.cookie',
+        bootstrap: 'assets/bootstrap/js/bootstrap.min',
+        template: 'assets/artTemplate/template-native'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        }
+    }
+});
---
+++ public/src/common.js
@@ -0,0 +1,30 @@
+define(['jquery', 'template', 'cookie'], function ($, template) {
+
+
+    $('.navs ul').prev('a').on('click', function () {
+        $(this).next().slideToggle();
+    });
+
+
+    // 通过检测cookie中有没有PHPSESSID来判断用户是否登录
+    // 除了登录页面以外都要进行检测
+
+    var pathname = location.pathname;
+    if (pathname != '/studyit1/login' && !$.cookie('PHPSESSID')) {
+        location.href = '/studyit1/login';
+    }
+
+    // 设置用户信息
+    var loginfo = $.cookie('loginfo') && JSON.parse($.cookie('loginfo'));
+
+    var source = '<!-- 头像 -->';
+    source += '<div class="avatar img-circle">';
+    source += '<img src="<%= tc_avatar %>">';
+    source += '</div>'
+    source += '<h4><%= tc_name %></h4>';
+
+    var render = template.compile(source);
+    var html = render(loginfo);
+
+    $('.aside .profile').html(html);
+})
